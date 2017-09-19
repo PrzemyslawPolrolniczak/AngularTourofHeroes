1b33fd9f2c8841e80cc8bc48cc0a3208963de023
Przemyslaw Polrolniczak <przemekp96@gmail.com>
2017-09-19 17:18:13 +0200

    raw template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+    id: 1,
+    name: 'batman'
+  }
+  heroes = HEROES;
+  selected: boolean;
+  isSelected(hero) {
+    this.selected = !this.selected;
+    return this.selected;
+  }
 }
+
+export class Hero {
+  name: string;
+  id: number;
+}
+
+const HEROES: Hero[] = [
+  {id: 1, name: 'Batman'},
+  {id: 2, name: 'Superman'},
+  {id: 3, name: 'Deadpool'},
+  {id: 4, name: 'WonderWoman'},
+  {id: 5, name: 'Spiderman'},
+  {id: 6, name: 'Flash'},
+  {id: 7, name: 'Wolverine'},
+  {id: 8, name: 'Hulk'},
+  {id: 9, name: 'Archer'},
+  {id: 10, name: 'Whatever'}
+]
